f0d408b12b5b2a206841b991fc819968f9e7eeb1
Jmmxp <Jmmxp@users.noreply.github.com>
2021-01-18 17:05:09 -0500

    Change comments, print anything sent to our sever program

--- echo_server.py
+++ echo_server.py
@@ -3,7 +3,7 @@
 import socket
 
 HOST = "" # Symbolic name meaning all available interfaces
-PORT = 8001
+PORT = 8001 # Chose this port as it is the one used in the lab questions.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
@@ -13,4 +13,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024)
             if not data: break
+
+            # Print anything that is sent to our server program.
+            print(data)
             conn.sendall(data)
