0883a7be6d1c756aefd63456e70190ad9692902f
Jmmxp <Jmmxp@users.noreply.github.com>
2021-01-18 18:39:48 -0500

    Use multiprocessing library to handle multiple clients at once for echo_server

--- echo_server.py
+++ echo_server.py
@@ -1,13 +1,9 @@
 # Code skeleton from official Python socket library documentation at https://docs.python.org/3/library/socket.html
 # Echo server program
 import socket
+from multiprocessing import Process
 
-HOST = "" # Symbolic name meaning all available interfaces
-PORT = 8001 # Chose this port as it is the one used in the lab questions.
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
+def handle_client(conn, addr):
     with conn:
         print("Connected by", addr)
         while True:
@@ -17,3 +13,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # Print anything that is sent to our server program.
             print(data)
             conn.sendall(data)
+            
+HOST = "" # Symbolic name meaning all available interfaces
+PORT = 8001 # Chose this port as it is the one used in the lab questions.
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+
+    # Listen for any incoming client connections until the server is forcefully shut-down.
+    while True:
+        conn, addr = s.accept()
+        p = Process(target=handle_client, args=(conn, addr,))
+        p.start()
+        p.join()
